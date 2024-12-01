8b7f89fa5a21ce08cb8dd6777adb2776e0b1b79a
Melat t <melattesfaye16@gmail.com>
2024-12-01 22:07:25 +0300

    file picker issue resolved

--- queue_manager_app/lib/features/auth/presentation/pages/signuppage.dart
+++ queue_manager_app/lib/features/auth/presentation/pages/signuppage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:queue_manager_app/features/auth/presentation/widgets/authButton.dart';
+import 'package:queue_manager_app/features/auth/presentation/widgets/filePicker.dart';
 import 'package:queue_manager_app/features/auth/presentation/widgets/mytextfield.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -11,57 +13,77 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  String? _fileName;
+
+  void _onFilePicked(String? fileName) {
+    setState(() {
+      _fileName = fileName;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Register',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.w800,
-              ),
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            MyTextField(
-                labelText: 'First Name',
-                controller: widget.nameController,
-                hintText: 'Name'),
-
-            const SizedBox(
-              height: 5,
-            ),
-            MyTextField(
-                labelText: 'Last Name',
-                controller: widget.nameController,
-                hintText: 'Name'),
-            const SizedBox(
-              height: 5,
-            ),
-            MyTextField(
-                labelText: 'Station',
-                controller: widget.nameController,
-                hintText: 'St.George'),
-            const SizedBox(
-              height: 5,
-            ),
-            MyTextField(
-                labelText: 'Phone Number',
-                controller: widget.nameController,
-                hintText: '0912345678'),
-
-            //Managed routes: from backend
-          ],
-        )),
-      ),
-    ));
+          child: Center(
+            child: SingleChildScrollView(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Register',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                MyTextField(
+                    labelText: 'First Name',
+                    controller: widget.nameController,
+                    hintText: 'Name'),
+                const SizedBox(
+                  height: 8,
+                ),
+                MyTextField(
+                    labelText: 'Last Name',
+                    controller: widget.nameController,
+                    hintText: 'Name'),
+                const SizedBox(
+                  height: 8,
+                ),
+                MyTextField(
+                    labelText: 'Station',
+                    controller: widget.nameController,
+                    hintText: 'St.George'),
+                const SizedBox(
+                  height: 8,
+                ),
+                MyTextField(
+                    labelText: 'Phone Number',
+                    controller: widget.nameController,
+                    hintText: '0912345678'),
+                const SizedBox(
+                  height: 12,
+                ),
+                const Text(
+                  'Upload ID',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                FileSelectorWidget(label: 'Upload Kebele ID'),
+                FileSelectorWidget(label: 'Upload Queue Manager ID'),
+                const AuthButton(label: 'Register')
+              ],
+            )),
+          ),
+        ));
   }
 }
