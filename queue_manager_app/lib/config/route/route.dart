672fa7b7585d1720f9ff84b36e73b3ebdf0cd518
Melat t <melattesfaye16@gmail.com>
2024-12-02 22:41:08 +0300

    routes: uses go_routes package to define routes.

---
+++ queue_manager_app/lib/config/route/route.dart
@@ -0,0 +1,16 @@
+import 'package:go_router/go_router.dart';
+import 'package:queue_manager_app/features/auth/presentation/pages/signinpage.dart';
+import 'package:queue_manager_app/features/auth/presentation/pages/signuppage.dart';
+import 'package:queue_manager_app/features/splash/splash.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/',
+    
+    routes: [
+    
+    GoRoute(path: '/', builder: (context, state) => const Splash()),
+    GoRoute(path: '/signin', builder: (context, state) => SigninPage()),
+    GoRoute(path: '/signup', builder: (context, state) => SignUpPage())
+  ]);
+}
