20f32cf3e7c88f75d28771bae85c2f0ee7d46cff
Melat t <melattesfaye16@gmail.com>
2025-01-06 13:12:27 +0300

    logout functionality and code optimization

--- Mobile/queue_manager_app/lib/main.dart
+++ Mobile/queue_manager_app/lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:queue_manager_app/config/route/route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:queue_manager_app/core/util/notification.dart';
-import 'package:queue_manager_app/features/auth/domain/usecase/api_service.dart';
-import 'package:queue_manager_app/features/auth/domain/usecase/auth_service.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -25,26 +23,6 @@ class _MyAppState extends State<MyApp> {
   // ignore: unused_field
   final NotificationService _notificationService = NotificationService();
   // final AuthenticationService _authService = AuthenticationService();
-  final ApiService _apiService = ApiService();
-
-  @override
-  void initState() {
-    super.initState();
-    _initializeApp();
-  }
-
-  Future<void> _initializeApp() async {
-    // final accessToken = await _authService.getAccessToken();
-    // final refreshToken = await _authService.getRefreshToken();
-    // print('Access Token: $accessToken');
-    // print('Refresh Token: $refreshToken');
-    // await _authService.saveTokens(accessToken!, refreshToken as String);
-
-    // ignore: unnecessary_null_comparison
-    // if (accessToken != null && refreshToken != null) {
-    //   await _apiService.sendTokensToBackend(accessToken, refreshToken);
-    // }
-  }
 
   @override
   Widget build(BuildContext context) {
