e89146d045075974e60bde608561a8eb562ec421
Melat t <melattesfaye16@gmail.com>
2025-01-11 22:31:32 +0300

    websocket connection implementation using riverpod

--- Mobile/queue_manager_app/lib/main.dart
+++ Mobile/queue_manager_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:queue_manager_app/config/provider/webSocket.dart';
 import 'package:queue_manager_app/config/route/route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:queue_manager_app/core/util/notification.dart';
@@ -9,20 +11,25 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  // ignore: unused_field
+class _MyAppState extends ConsumerState<MyApp> {
   final NotificationService _notificationService = NotificationService();
-  // final AuthenticationService _authService = AuthenticationService();
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the WebSocket connection
+    ref.read(webSocketProvider);
+  }
 
   @override
   Widget build(BuildContext context) {
