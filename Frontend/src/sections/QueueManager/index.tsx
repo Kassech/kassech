aadef2b75a20b4b9a4f605ecd338c763d771feac
BettyyN <betelhemnegash10@gmail.com>
2025-01-13 23:29:29 +0300

    Queue Manager Registration

---
+++ Frontend/src/sections/QueueManager/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QueueManagerForm from './queueManagerForm';
+
+const Vehicle: React.FC = () => {
+  return (
+    <div>
+      <QueueManagerForm />
+    </div>
+  );
+};
+
+export default Vehicle;
