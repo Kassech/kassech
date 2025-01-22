951190ee996151b1c6e3fc24273478b3a15f3798
bishopone <abeselomsolomon106@gmail.com>
2025-01-23 01:06:31 +0300

    user verification completed (/users/verify/:id)

--- Backend/pkg/controller/user_controller.go
+++ Backend/pkg/controller/user_controller.go
@@ -433,6 +433,33 @@ func (uc *UserController) UpdateUser(c *gin.Context) {
 	})
 }
 
+func (uc *UserController) VerifyUser(c *gin.Context) {
+	userId := c.Param("id")
+	state := c.Query("state")
+	stateBool, err := strconv.ParseBool(state)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid state"})
+		return
+	}
+
+	userIdUint, err := utils.StringToUint(userId)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	updatedUser, err := uc.Service.VerifyUser(userIdUint, stateBool)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User verified successfully",
+		"user":    updatedUser,
+	})
+}
+
 // DeleteUser method (Delete User by ID)
 func (uc *UserController) DeleteUser(c *gin.Context) {
 	userId := c.Param("id")
