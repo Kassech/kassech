86549eed3f56cea8b0b769a3e4aee7dac370910c
BettyyN <betelhemnegash10@gmail.com>
2025-02-03 22:49:22 +0300

    Bug Fix

--- Frontend/src/pages/driver/index.tsx
+++ Frontend/src/pages/driver/index.tsx
@@ -6,6 +6,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { driverAttachmentSchema, driverSchema } from '@/types/schemas';
 import { z } from 'zod';
 import { DRIVER_ROLE } from '@/constants';
+import DriverForm from '@/sections/Driver/driverForm';
+import DriverAttachmentForm from '@/sections/Driver/driverAttacchmentForm';
 
 export default function DriverPage({
   defaultValues={},
